1f5f865372a398a72c4e3d236eb3ea8271becfb7
MuhammadWaleedEjaz21 <m.waleedejaz2003@gmail.com>
2025-12-05 22:27:27 +0500

    first commit

---
+++ frontend/lib/Models/task_model.dart
@@ -0,0 +1,63 @@
+class TaskModel {
+  final String userEmail;
+  final String title;
+  final String description;
+  final String category;
+  final String priority;
+  final DateTime dueDate;
+  final bool isCompleted;
+
+  TaskModel({
+    required this.userEmail,
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.priority,
+    required this.dueDate,
+    required this.isCompleted,
+  });
+
+  factory TaskModel.fromJson(Map<String, dynamic> json) {
+    return TaskModel(
+      userEmail: json['user_email'],
+      title: json['title'],
+      description: json['description'],
+      category: json['category'],
+      priority: json['priority'],
+      dueDate: DateTime.parse(json['due_date']),
+      isCompleted: json['is_completed'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'user_email': userEmail,
+      'title': title,
+      'description': description,
+      'category': category,
+      'priority': priority,
+      'due_date': dueDate.toIso8601String(),
+      'is_completed': isCompleted,
+    };
+  }
+
+  TaskModel copyWith({
+    String? userEmail,
+    String? title,
+    String? description,
+    String? category,
+    String? priority,
+    DateTime? dueDate,
+    bool? isCompleted,
+  }) {
+    return TaskModel(
+      userEmail: userEmail ?? this.userEmail,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      category: category ?? this.category,
+      priority: priority ?? this.priority,
+      dueDate: dueDate ?? this.dueDate,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+}
---
+++ frontend/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:frontend/Screens/home_screen.dart';
+
+void main() {
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(393, 852),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: HomeScreen(),
+      builder: (context, child) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'FocusFlow',
+        home: child,
+      ),
+    );
+  }
+}
