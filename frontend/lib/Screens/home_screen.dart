529cb9ca65a000da3e1fb4499bff1c36aa8fdf94
MuhammadWaleedEjaz21 <m.waleedejaz2003@gmail.com>
2025-12-15 23:04:30 +0500

    refactor(home): remove unnecessary line and update task service base URL to production

--- frontend/lib/Screens/home_screen.dart
+++ frontend/lib/Screens/home_screen.dart
@@ -9,7 +9,6 @@ import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
